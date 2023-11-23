f3ace1f2228837ad8177bf168610b71051975353
bhashimoto <brunothashimoto@gmail.com>
2023-11-23 15:07:06 -0300

    added dynamic routing and some responsiveness

--- angular-blog/src/app/pages/home/home.component.ts
+++ angular-blog/src/app/pages/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { dataFake } from '../../../data/dataFake';
+
+
+  type Post =
+    {
+      id:string, 
+      photo:string, 
+      title:string, 
+      description:string
+    };
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css','./home.responsive.component.css']
 })
 export class HomeComponent implements OnInit {
-
   constructor() { }
+  @Input()
+  bigCard:Post = {id:"", photo:"", title:"", description:""};
+  @Input()
+  smallCards:Post[] = [{id:"", photo:"", title:"", description:""}];
+  private smallCardsAmount = 3;
 
   ngOnInit(): void {
+    this.bigCard = dataFake[dataFake.length - 1];
+    console.log(this.bigCard);    
+
+    this.smallCards = dataFake.slice(dataFake.length -1 - this.smallCardsAmount, dataFake.length - 1).reverse();
+
   }
 
 }
---
+++ angular-blog/src/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6bnOy_uOhsVTSvyenkI4eoclbmzQyMcZvpw&usqp=CAU",
+        "title": "saiu o novo filme",
+        "description": "ola",
+    },
+    {
+        "id": "2",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3tIKAQJOPYfNadTZ1K43MDok_W24ZmLHU7A&usqp=CAU",
+        "title": "saiu outro filme",
+        "description": "ola ola",
+    },
+    {
+        "id": "3",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzQ1SYFwg9anXjvRTPtbFmBVPaIqHuqJglxw&usqp=CAU",
+        "title": "Tá saindo mais filme",
+        "description": "ola",
+    },
+    {
+        "id": "4",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQymU18Z6SeGhGz4VAj8bLxZCcvWIPfRmq8qQ&usqp=CAU",
+        "title": "meu deus, quanto filme",
+        "description": "ola",
+    },
+    {
+        "id": "5",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmKYOgJ5jkOhxyjcH1OUSQjoVQ-hWKMp_E9A&usqp=CAU",
+        "title": "Cansei de filme",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquam lectus ac enim sagittis, in fermentum erat finibus. Nam consectetur, tellus ac tincidunt gravida, tellus sem molestie sapien, quis dapibus tortor erat porttitor libero. Nulla elementum dignissim lectus, ac porta purus auctor vitae. Curabitur faucibus consectetur leo vel rutrum. Fusce et vehicula arcu. Suspendisse dolor ligula, luctus eget ipsum vitae, malesuada mollis tellus. Mauris nulla augue, sagittis quis auctor non, cursus et leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed bibendum ac ligula vel congue. Morbi in justo ante. Proin a augue tellus. Curabitur molestie urna sed neque posuere bibendum.",
+    },
+]
